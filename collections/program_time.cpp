9e27c0825275af07d36c8beec1d3abae49f2d76a
Asif <asifzamanzisan@gmail.com>
2025-09-10 20:19:45 +0600

    cpp code chunk to measure program time

---
+++ collections/program_time.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ll long long
+#define MAX 1e9+7
+
+int main () {
+  clock_t start = clock();
+
+  /*------------
+  // program body
+  -----------*/
+ 
+  clock_t end = clock();
+  cerr << "Program Speed " << 1.0 *  ((double)(end - start)/(double)CLOCKS_PER_SEC) << "s\n";
+  return 0;
+}
