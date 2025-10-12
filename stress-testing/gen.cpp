c4150dc9e1180625f8435a3b4a3b0dfe466bb7cf
Asif <asifzamanzisan@gmail.com>
2025-10-12 23:18:10 +0600

    Update gen.cpp

--- stress-testing/gen.cpp
+++ stress-testing/gen.cpp
@@ -5,18 +5,33 @@ using namespace std;
 #define tensho cin.tie(nullptr);
 using ll = long long;
 
-int generate(int a, int b) {
+long long generate(int a, int b) {
   return a + rand() % (b - a + 1);
+}
+int generate(int a) {
+  return rand() % a;
+}
+
+// generate array with non-unique elements (can be similar)
+vector<int> non_unique(ll n, int range) {
+  vector<int> pool, final;
+  for (int i = 0; i < n; i++) {
+    pool.push_back(generate(1, range));
+  }
+  int pool_size = pool.size();
+  for (int i = 0; i < n; i++) {
+    final.push_back(pool[generate(0, pool_size-1)]);
+  }
+  sort(final.begin(), final.end());
+  return final;
 }
 
 int main(int argc, char *argv[]) {
   srand(atoi(argv[1]));
   freopen("input.txt", "w", stdout);
-  // n, m -10^5, 10^5
-  ll n = generate(-1e5, 1e5);
-  ll m = generate(-1e5, 1e5);
   
-  cout << n << ' ' << m;
   
+  
+  puts("");
   return 0;
 }
